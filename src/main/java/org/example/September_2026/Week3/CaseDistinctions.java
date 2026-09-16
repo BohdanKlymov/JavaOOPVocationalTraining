40b75c07ddd0e91875056d5f7cd7720c42bcb1cd
Klymov <bohdan.klymov@symmedia.de>
2026-09-16 15:09:50 +0200

    first commit

---
+++ src/main/java/org/example/September_2026/Week3/CaseDistinctions.java
@@ -0,0 +1,26 @@
+package org.example.September_2026.Week3;
+
+public class CaseDistinctions {
+
+//    Aufgabe 2: Fallunterscheidungen (20 Punkte).
+//    Der Dortmunder Tierpark hat beschlossen, ein Programm schreiben zu lassen, das Besuchern
+//    Auskünfte über die im Zoo vorhandenen Tiere geben kann. Die Implementierung übernimmt
+//    das Startup symmediaAzubis, welches aus Auszubildene der Fachinformatik-Anwendungsentwicklung der symmedia GmbH besteht, die
+//    gerade ihre ersten Java-Kenntnisse erworben haben. In der ersten Ausbaustufe gibt ihre Implementierung zu einem Tier lediglich einige elementare Eigenschaften aus. Wird das Programm
+//    mit dem Parameter „Spinne“ aufgerufen, lautet die Ausgabe
+//
+//8 Beine
+//    flugfähig: nein
+//    hat Federn: nein
+//    gefährlich für Menschen: eventuell
+//
+//    In seiner Implementierung benutzt das Team Fallunterscheidungen.
+//            a) Schreiben Sie ein solches Programm, wie es das Team getan
+//                  hat. Es sollte für insgesamt 10 verschiedene Tiere Auskunft über die obigen vier Eigenschaften liefern können. Die 10 Tie sind die oben beschriebene Spinne und 9 weitere frei wählbare.
+//                  Achtung: Bei dieser Aufgabe geht es noch nicht um objektorientierte Programmierung, sondern um Java-Grundlagen. Lösen Sie die Aufgabe daher rein imperativ mit nur einer Klasse,
+//                  in deren main-Methode sich das komplette Programm befindet.
+//            b) Halten Sie das Vorgehen für praxistauglich? Wo sehen Sie Fehlerquellen?
+//                  (Eine Lösung zur Vermeidung dieser Fehlerquellen brauchen Sie nicht benennen. Diese werden Sie später im Kurs kennenlernen. Wenn Sie möchten, können Sie natürlich auch jetzt schon darüber nachdenken.)
+//            c) schreiben Sie eine alternative Lösung, in der die Klasse Tier implementiert und verwendet wird. Male auch das Klassendiagramm zu der Klasse Tier auf Papier.
+
+}
